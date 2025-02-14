7b73d748b980e57fc267804001a748450b18ba05
1stormwave1 <laitaruk777@gmail.com>
2025-02-14 22:53:18 +0200

    Added base for building PCG

---
+++ Source/HorrorGame/Public/Building/PCGSettings.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "PCGSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORGAME_API UPCGSettings : public UObject
+{
+	GENERATED_BODY()
+	
+};
