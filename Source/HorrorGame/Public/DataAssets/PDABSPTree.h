0097c26614b978a469c43bd44fdf98418784d20b
1stormwave1 <laitaruk777@gmail.com>
2025-05-28 19:37:25 +0300

    Added first changed for BSP trees of PCG

---
+++ Source/HorrorGame/Public/DataAssets/PDABSPTree.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "PDABSPTree.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORGAME_API UPDABSPTree : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};
