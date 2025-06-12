34d9da1c7e35b250ad4631820d24edbbb187b9e7
1stormwave1 <laitaruk777@gmail.com>
2025-06-12 18:47:10 +0300

    Added base for genetic algorithm

---
+++ Source/HorrorGame/Public/Building/RoomTraitTree.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "RoomTraitTree.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORGAME_API URoomTraitTree : public UObject
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/HorrorGame/Public/Building/TraitGeneticAlgorithm.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "TraitGeneticAlgorithm.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORGAME_API UTraitGeneticAlgorithm : public UObject
+{
+	GENERATED_BODY()
+	
+};
