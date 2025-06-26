d20c10a5268df0f53c27e9fc2b192d646d203c84
1stormwave1 <laitaruk777@gmail.com>
2025-06-26 20:59:50 +0300

    added base for ai enemies and new meshes pack MonsterForSurvivalGame

--- Source/HorrorGame/Private/AI/EnemyController.cpp
+++ Source/HorrorGame/Private/AI/EnemyController.cpp
@@ -1,5 +1,69 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+
 
 
 #include "AI/EnemyController.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Navigation/CrowdFollowingComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
 
+AEnemyController::AEnemyController(const FObjectInitializer& ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	BehaviorTreeComponent = CreateDefaultSubobject<UBehaviorTreeComponent>(TEXT("Behavior Tree Component"));
+	BlackboardComponent = CreateDefaultSubobject<UBlackboardComponent>(TEXT("Blackboard Component"));
+
+	AIPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AI Perception Component"));
+
+	Sight = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("Sight Sense"));
+	if(Sight)
+	{
+		Sight->SightRadius = 500;
+		Sight->LoseSightRadius = 600;
+		Sight->PeripheralVisionAngleDegrees = 180.0f;
+		Sight->DetectionByAffiliation.bDetectEnemies = true;
+		Sight->DetectionByAffiliation.bDetectNeutrals = true;
+		Sight->DetectionByAffiliation.bDetectFriendlies = true;
+
+		AIPerceptionComponent->ConfigureSense(*Sight);
+	}
+}
+
+UBehaviorTreeComponent* AEnemyController::GetBehaviorTreeComponent() const
+{
+	return BehaviorTreeComponent;
+}
+
+void AEnemyController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	PerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &AEnemyController::OnActorSensed);
+	if (IsValid(BlackboardData))
+	{
+		UseBlackboard(BlackboardData, BlackboardComponent);
+	}
+
+	if (IsValid(BehaviorTree))
+	{
+		RunBehaviorTree(BehaviorTree);
+		BehaviorTreeComponent->StartTree(*BehaviorTree);
+	}
+}
+
+void AEnemyController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	if (IsValid(Blackboard) && IsValid(BehaviorTree))
+	{
+		Blackboard->InitializeBlackboard(*BehaviorTree->BlackboardAsset);
+	}
+}
+
+void AEnemyController::OnActorSensed_Implementation(AActor* Actor, FAIStimulus Stimulus)
+{
+}
--- Source/HorrorGame/Public/AI/EnemyController.h
+++ Source/HorrorGame/Public/AI/EnemyController.h
@@ -1,17 +1,51 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "Perception/AIPerceptionTypes.h"
 #include "EnemyController.generated.h"
 
-/**
- * 
- */
+
+class UAISenseConfig_Sight;
+class UBehaviorTreeComponent;
+
 UCLASS()
 class HORRORGAME_API AEnemyController : public AAIController
 {
 	GENERATED_BODY()
+
+public:
+	AEnemyController(const FObjectInitializer& ObjectInitializer);
+
+	UBehaviorTreeComponent* GetBehaviorTreeComponent() const;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void OnPossess(APawn* InPawn) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="AI|Perception")
+	UAISenseConfig_Sight* Sight = nullptr;
+
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "AI", meta = (AllowPrivateAccess = true))
+	UBehaviorTree* BehaviorTree;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "AI", meta = (AllowPrivateAccess = true))
+	UBlackboardData* BlackboardData;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI", meta = (AllowPrivateAccess = true))
+	UBehaviorTreeComponent* BehaviorTreeComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI", meta = (AllowPrivateAccess = true))
+	UBlackboardComponent* BlackboardComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI", meta = (AllowPrivateAccess = true))
+	UAIPerceptionComponent* AIPerceptionComponent;
+	
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void OnActorSensed(AActor* Actor, FAIStimulus Stimulus);
 	
 };
