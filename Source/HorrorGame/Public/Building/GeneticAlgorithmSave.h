8458a9cba33068d86fe0761dceeea5e3d5fc64de
1stormwave1 <laitaruk777@gmail.com>
2025-07-06 22:38:51 +0300

    added genetic algorithm initialization and core execution

---
+++ Source/HorrorGame/Public/Building/GeneticAlgorithmSave.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "GeneticAlgorithmSave.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORGAME_API UGeneticAlgorithmSave : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
